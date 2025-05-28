e2ce4b17de26e528c30985bc85e31756ab5a2e67
Marco2026 <marherluj@alum.us.es>
2025-05-28 22:48:56 +0200

    feat: added board class

--- othello/board.py
+++ othello/board.py
@@ -1,26 +1,28 @@
-import pygame
-
-# Constants for the game board
-WIDTH = 800
-HEIGHT = 800
-ROWS = 8
-COLS = 8
-SQUARE_SIZE = WIDTH // COLS
-
-# Colors used in the game in rgb format
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-GREEN = (20,134,58)
+import pygame as pg
+from .game_config import WIDTH, HEIGHT, ROWS, COLS, SQUARE_SIZE
+from .colors import BLACK, GREEN
 
 class Board:
     def __init__(self):
-        self.board = [[],
-                      []]
+        self.board = []
         self.turn = 0
         self.selected_piece = None
         self.possible_placements = None
         self.white_pieces = 2
         self.black_pieces = 2
 
-    def place_piece(self):
+    def draw_board(self, screen):
+        screen.fill(GREEN)
+
+        for i in range(ROWS + 1):
+            pg.draw.line(screen, BLACK, (0, i * SQUARE_SIZE), (WIDTH, i * SQUARE_SIZE), 2)
+        
+        for j in range (COLS + 1):
+            pg.draw.line(screen, BLACK, (j * SQUARE_SIZE, 0), (j * SQUARE_SIZE, HEIGHT), 2)
+
+    def draw_pieces(self, screen):
         pass
+
+    def place_piece(self, position):
+        self.board[position[0]][position[1]] = self.selected_piece
+        self.selected_piece = None
