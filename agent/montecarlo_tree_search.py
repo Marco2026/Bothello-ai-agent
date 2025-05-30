e1764333dec8545e4c9ebefe7f6b011177b7f2aa
Marco2026 <marherluj@alum.us.es>
2025-05-31 01:17:37 +0200

    feat: progress made in montecarlo tree search algorithm

--- agent/montecarlo_tree_search.py
+++ agent/montecarlo_tree_search.py
@@ -1,12 +1,16 @@
 import time
 from hyperparameters import TIME_TO_SEARCH, CONSTANT_C
 import math
+import random
 
 class Node:
     def __init__(self):
         self.children = []
         self.score = 0
         self.visits = 0
+        self.isTerminal = False
+        self.isExpanded = False
+        self.actions = []
 
     def calculate_average_value(self):
         res = 1000000
@@ -17,17 +21,42 @@ class Node:
     def calculate_upper_confidence_bound(self, state):
         return state.calculate_average_value() + 2 * CONSTANT_C * math.sqrt(2 * math.log(self.visits) / state.visits)
 
-def montecarlo_tree_search(state):
-    tree = Node(state)
+
+def uct_search(state):
+    root = Node(state)
     time_elapsed = time.time()
     time_limit = time.time() + TIME_TO_SEARCH
     while is_time_remaining(time_elapsed, time_limit):
-        leaf = select(tree)
-        child = expand(leaf)
-        result = simulate(child)
-        back_propagate(result, child)
-        time_elapsed = time.time()
-    return action
+        leaf = tree_policy(root)
+        score = default_policy(leaf)
+        backup(leaf, score)
+    return action(best_child(root, 0))
+
+def tree_policy(node):
+    while node.isTerminal == False:
+        if node.isExpanded == False:
+            return expand(node)
+        else:
+            node = best_child(node, CONSTANT_C)
+
+def expand(node):
+    for a in node.actions:
+        pass
+
+def default_policy(state):
+    while state.isTerminal == False:
+        action = random.choice(state.actions)
+        state = next_state(state, action)
+    return state.score
+
+def backup(node, score):
+    while node != None:
+        node.visits += 1
+        node.score += score
+
+def next_state(state, action):
+    # Devolver el estado resultante de aplicar una accion al estado actual (conexion con el juego)
+    pass
 
 def is_time_remaining(actual_moment, time_limit):
     return (time_limit - actual_moment) > 0
