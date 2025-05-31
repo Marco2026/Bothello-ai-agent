7d71f4f38f90c239ca7fefd9aa880b8d863d2a62
Marco2026 <marherluj@alum.us.es>
2025-05-31 19:07:28 +0200

    feat: added rendered text to the screen to display useful information

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import pygame as pg
 from othello.board import Board
-from othello.game_config import WIDTH, HEIGHT, SQUARE_SIZE
-
-# Constants for the game
-FPS = 60
-NAME = "Othello Game"
-SIZE = (WIDTH, HEIGHT)
+from othello.game_config import SCREEN_WIDTH, SCREEN_HEIGHT, SQUARE_SIZE, BOARD_HEIGHT, BOARD_WIDTH
+from othello.colors import BLACK
 
 pg.init()
+pg.font.init()
+
+FPS = 60
+NAME = "Othello Game"
+SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
+FONT = pg.font.Font("othello/assets/bahnschrift.ttf", 30)
+
 pg.display.set_caption(NAME)
 screen = pg.display.set_mode(SIZE)
 
@@ -26,6 +29,11 @@ def main():
 
     while running:
         clock.tick(FPS)
+        turn_information = FONT.render(f"The turn is: {board.turn}", True, BLACK)
+        current_player_information = FONT.render(f"Current player: {get_color_from_rgb(board.current_player)}", True, BLACK)
+        white_pieces_information = FONT.render(f"White pieces: {board.white_pieces}", True, BLACK)
+        black_pieces_information = FONT.render(f"Black pieces: {board.black_pieces}", True, BLACK)
+
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 running = False
@@ -36,10 +44,21 @@ def main():
                 board.put_piece(row, col)
 
         board.draw_screen(screen)
+        screen.blit(turn_information, (10, BOARD_HEIGHT + 10))
+        screen.blit(current_player_information, (10, BOARD_HEIGHT + 40))
+        screen.blit(black_pieces_information, (BOARD_WIDTH - 250, BOARD_HEIGHT + 10))
+        screen.blit(white_pieces_information, (BOARD_WIDTH - 250, BOARD_HEIGHT + 40))
 
         pg.display.flip()
 
-
     pg.quit()
+
+def get_color_from_rgb(rgb_color):
+    res = "unknown color"
+    if rgb_color == (0, 0, 0):
+        res = "Black"
+    elif rgb_color == (255, 255, 255):
+        res = "White"
+    return res 
 
 main()
