86b1c16349678518621ce24018585e644922cda0
Marco2026 <marherluj@alum.us.es>
2025-06-09 17:14:21 +0200

    fix: wrong model

--- agent/hyperparameters.py
+++ agent/hyperparameters.py
@@ -2,4 +2,4 @@ import math
 
 TIME_TO_SEARCH = 1. # In seconds
 CONSTANT_C = 1 / math.sqrt(2)
-NEURAL_NETWORK_MODEL = 'agent/training/models/bothello.h5'
+NEURAL_NETWORK_MODEL = 'agent/training/models/bothello_first_training.h5'
